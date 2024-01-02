8623fc7ad5ad615a25de52a7cb224df1e7909bcb
melvintivane <melvinshuster47@gmail.com>
2024-01-02 17:24:14 +0200

    chore: :wrench: configure eslint and prettier

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
