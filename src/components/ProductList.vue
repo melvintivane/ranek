e54c313ce4b5f52e5ab0b0fc33cf1abc487acafc
melvintivane <melvinshuster47@gmail.com>
2024-01-02 22:40:54 +0200

    chore: create ProductList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container"></section>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  data() {
+    return {
+      products: null,
+    }
+  },
+  methods: {
+    getProducts() {
+      fetch('http://localhost:3000/product')
+        .then((res) => res.json())
+        .then((res) => (this.products = res))
+    },
+  },
+  created() {
+    this.getProducts()
+  },
+}
+</script>
+
+<style scoped></style>
