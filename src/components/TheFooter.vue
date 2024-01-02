afa21ff5085586f6abfb0cf7be636072f596aba9
melvintivane <melvinshuster47@gmail.com>
2024-01-02 21:44:27 +0200

    chore: created Footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Ranek. Todos os direitos reservados</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
+
+<style scoped>
+footer {
+  position: relative;
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  top: 75px;
+}
+</style>
