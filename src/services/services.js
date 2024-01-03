222b62cd0eca917428800582f6d9193ab3990977
melvintivane <melvinshuster47@gmail.com>
2024-01-03 10:25:50 +0200

    feat: add axios instance

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000',
+})
+
 export const api = {
   get(url) {
-    return axios.get(url)
+    return axiosInstance.get(url)
   },
 }
