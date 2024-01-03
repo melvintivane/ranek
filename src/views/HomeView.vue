261b3a4aead8b8dcafe1c563d2ecc490aeeea43e
melvintivane <melvinshuster47@gmail.com>
2024-01-03 09:40:00 +0200

    style: Enhanced the layout and styling in HomeView.vue for a more appealing presentation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍Compre ou Venda</h1>
+    <FindProducts />
+    <ProductList />
+  </section>
 </template>
 
 <script>
+import FindProducts from '@/components/FindProducts.vue'
+import ProductList from '@/components/ProductList.vue'
+
 export default {
-  name: "HomeView",
-};
+  name: 'HomeView',
+  components: {
+    FindProducts,
+    ProductList,
+  },
+}
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
