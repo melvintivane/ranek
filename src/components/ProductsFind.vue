3e61146e9407bb145ca8f6d5b0413831b80c7eff
melvintivane <melvinshuster47@gmail.com>
2024-01-03 11:52:15 +0200

    feat: Added search functionality to ProductsFind.vue, enhancing user interaction.

--- src/components/ProductsFind.vue
+++ src/components/ProductsFind.vue
@@ -1,9 +1,24 @@
 <template>
-  <input type="text" />
+  <form action="">
+    <input name="search" id="search" type="text" v-model="search" />
+    <input id="magnify" type="submit" @click.prevent="handleSearch" />
+  </form>
 </template>
 <script>
 export default {
   name: 'ProductsFind',
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    handleSearch() {
+      this.$router
+        .push({ query: { q: this.search } })
+        .catch((error) => console.error(error))
+    },
+  },
 }
 </script>
 <style scoped></style>
