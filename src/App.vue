bd3d799600723e110e69d980d3012699355948df
melvintivane <melvinshuster47@gmail.com>
2024-01-04 17:17:28 +0200

    style: add transition styles
    Added transition styles for smoother animations during element enter and
    leave actions. The styles include opacity changes and translations to
    create a fade-in and fade-out effect with a slight vertical movement.

--- src/App.vue
+++ src/App.vue
@@ -102,4 +102,22 @@ textarea:hover,
 textarea:focus {
   border-color: #87f;
 }
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter {
+  transform: translate3d(0, -20px, 0);
+}
+
+.v-leave-to {
+  transform: translate3d(0, 20px, 0);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
+}
 </style>
