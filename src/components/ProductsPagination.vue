7ff96dd80c8b0075db5b93cdb7a53881c3578d1a
melvintivane <melvinshuster47@gmail.com>
2024-01-04 15:56:28 +0200

    feat: Implement ProductsPagination component for pagination
    - Added ProductsPagination component to handle pagination logic.
    - The component receives the number of products per page (`productsPerPage`) and the total number of products (`productsTotal`) as props.
    - Implemented the calculation of total pages, current page, and the range of pages to be displayed.
    - The component renders a list of page links and a "Next" link for navigating to the next page.
    - Applied styling to the pagination links for better visibility.

--- src/components/ProductsPagination.vue
+++ src/components/ProductsPagination.vue
@@ -1,7 +1,80 @@
-<template></template>
+<template>
+  <ul v-if="totalPages > 1">
+    <li v-for="page in pages" :key="page">
+      <router-link :to="{ query: query(page) }">{{ page }}</router-link>
+    </li>
+    <router-link :to="{ query: { _page: currentPage + 1 } }">Next</router-link>
+  </ul>
+</template>
 
 <script>
-export default {}
+export default {
+  props: {
+    productsPerPage: {
+      type: Number,
+      default: 1,
+    },
+    productsTotal: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    pages() {
+      const current = Number(this.$route.query._page)
+      const range = 9
+      const offset = Math.ceil(range / 2)
+      const total = this.totalPages
+      const pagesArray = []
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i)
+      }
+
+      //OFFSET OF PAGES
+      pagesArray.splice(0, current - offset)
+      pagesArray.splice(range, total)
+
+      return pagesArray
+    },
+    totalPages() {
+      const total = this.productsTotal / this.productsPerPage
+      return total !== Infinity ? Math.ceil(total) : 0
+    },
+    currentPage() {
+      const currentPage = Number(this.$route.query._page)
+      return currentPage
+    },
+  },
+  methods: {
+    query(page) {
+      return {
+        ...this.$route.query,
+        _page: page,
+      }
+    },
+  },
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  grid-column: 1 / -1;
+}
+
+li {
+  display: inline-block;
+}
+
+li a {
+  padding: 2px 8px;
+  border-radius: 2px;
+  margin: 4px;
+}
+
+li a.router-link-exact-active,
+li a:hover {
+  background: #87f;
+  color: #fff;
+}
+</style>
