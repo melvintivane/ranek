16246dde32c32d01dd48eac2e976739c22e0c143
melvintivane <melvinshuster47@gmail.com>
2024-01-04 17:27:06 +0200

    chore: created Product component

---
+++ src/views/ProductView.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>Product</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ProductView',
+  components: {},
+}
+</script>
+
+<style scoped></style>
