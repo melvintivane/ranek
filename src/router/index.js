b1e8106fe32e5d4a7dc1b46b368ecf36343a4d79
melvintivane <melvinshuster47@gmail.com>
2024-01-05 14:07:47 +0200

    feat: add route and component for dynamic product view
    Added a new route in the router configuration to handle dynamic
    product views based on the product ID. Also, created a ProductView
    component to display detailed information about a specific product.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    props: true,
+    component: ProductView,
   },
 ]
 
