a42652c6895b59a9a752e4595e030d5f70b3880d
melvintivane <melvinshuster47@gmail.com>
2024-01-06 08:33:05 +0200

    style: update LoginView layout
    Updated the layout of the LoginView by organizing form elements, adding labels, and creating space for a "Forget password" link. Also, added a LoginCreate component for user registration.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,42 @@
 <template>
-  <div class="">Login</div>
+  <section class="login">
+    <h1>Login</h1>
+    <form>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" v-model="login.email" />
+      <label for="password">password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="login.password"
+      />
+      <button class="btn" @click.prevent="logon">Login</button>
+    </form>
+    <p class="lost">
+      <a href="/" target="_blank">Forget password? Click here.</a>
+    </p>
+    <LoginCreate />
+  </section>
 </template>
 
 <script>
+import LoginCreate from '@/components/LoginCreate.vue'
+
 export default {
   name: 'loginView',
+  components: { LoginCreate },
+  data() {
+    return {
+      login: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    logon() {},
+  },
 }
 </script>
 
