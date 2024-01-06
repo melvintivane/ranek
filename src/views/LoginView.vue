8c3c433c89a38d21681d38c078a37f4548a1b57c
melvintivane <melvinshuster47@gmail.com>
2024-01-06 09:09:55 +0200

    style(login): improve login view styling
    Adjusted the styling of the LoginView component for a better visual experience. Added placeholders for email and password inputs, centered the form, and enhanced the overall layout.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,11 +3,18 @@
     <h1>Login</h1>
     <form>
       <label for="email">Email</label>
-      <input type="email" name="email" id="email" v-model="login.email" />
-      <label for="password">password</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter email..."
+        id="email"
+        v-model="login.email"
+      />
+      <label for="password">Password</label>
       <input
         type="password"
         name="password"
+        placeholder="Enter password..."
         id="password"
         v-model="login.password"
       />
@@ -40,4 +47,46 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.login {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+}
+
+form {
+  display: grid;
+}
+
+#email,
+#password {
+  width: 100%;
+  padding: 20px;
+  border: 1px solid #87f;
+  outline-color: #87f;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.lost {
+  text-align: center;
+  margin: 20px auto 0 auto;
+}
+
+.lost:hover {
+  color: #87f;
+  text-decoration: underline;
+}
+</style>
