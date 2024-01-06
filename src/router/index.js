dd1cf5d782a8d8d0ed0206e0c83870052839e251
melvintivane <melvinshuster47@gmail.com>
2024-01-06 08:33:40 +0200

    feat: add LoginView route
    Added a route for the LoginView component at '/login'.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 import ProductView from '../views/ProductView.vue'
 
 Vue.use(VueRouter)
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
   },
   {
     path: '/product/:id',
