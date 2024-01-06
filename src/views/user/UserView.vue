b43f06104bc51e5f02ce661928f5a971e942e624
melvintivane <melvinshuster47@gmail.com>
2024-01-06 18:03:06 +0200

    chore: created LoginView component

---
+++ src/views/user/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>{{ user }}</section>
+</template>
+
+<script>
+export default {
+  name: 'userView',
+  computed: {
+    user() {
+      return this.$store.state.user.name
+    },
+  },
+}
+</script>
+
+<style scoped></style>
