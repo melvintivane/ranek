80261460db297de2aedad477ffc3d0b0b98ce1b6
melvintivane <melvinshuster47@gmail.com>
2024-01-07 14:32:23 +0200

    feat(user): implement basic UserForm component
    Implemented a basic UserForm component with form fields for name, email, password, address, state, and city. Used Vuex to manage the state of the form and synchronize it with the store. Added styling to organize the form fields in a grid layout.

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,11 +1,77 @@
 <template>
-  <section>UserForm</section>
+  <form>
+    <label for="name">Name</label>
+    <input
+      autocomplete="name"
+      type="text"
+      id="name"
+      name="name"
+      v-model="username"
+    />
+    <!-- <label for="email">Email</label>
+    <input
+      autocomplete="email"
+      type="email"
+      id="email"
+      name="email"
+      v-model="email"
+    />
+    <label for="password">Password</label>
+    <input
+      autocomplete="password"
+      type="password"
+      id="password"
+      name="password"
+      v-model="password"
+    />
+    <label for="address">Address</label>
+    <input
+      autocomplete="address"
+      type="text"
+      id="address"
+      name="address"
+      v-model="address"
+    />
+    <label for="state">State</label>
+    <input
+      autocomplete="state"
+      type="text"
+      id="state"
+      name="state"
+      v-model="state"
+    />
+    <label for="city">City</label>
+    <input
+      autocomplete="city"
+      type="text"
+      id="city"
+      name="city"
+      v-model="city"
+    /> -->
+    <div>
+      <slot></slot>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
   name: 'userForm',
+  computed: {
+    username: {
+      get() {
+        return this.$store.state.name
+      },
+      set(value) {
+        this.$store.commit('UPDATE_USER', { name: value })
+      },
+    },
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  display: grid;
+}
+</style>
